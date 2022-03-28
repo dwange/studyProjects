e9f605cd90927a9274823b57f2ce5cfc6f5e0fc4
Ekaterina <dwange@yandex.ru>
2022-03-28 13:04:02 +0300

    First commit

---
+++ Cookcademy/Cookcademy/CookcademyApp.swift
@@ -0,0 +1,19 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Kate on 12.11.2021.
+//
+
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+           MainTabView()
+        }
+    }
+}
+
---
+++ Cookcademy/Cookcademy/Views/MainTabView.swift
@@ -0,0 +1,36 @@
+//
+//  MainTabView.swift
+//  Cookcademy
+//
+//  Created by Kate on 27.02.2022.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+   
+    @StateObject var recipeData = RecipeData()
+   
+    var body: some View {
+        TabView {
+        RecipeCategoryGridView()
+          .tabItem { Label("Recipes", systemImage: "list.dash") }
+        NavigationView {
+            RecipesListView(viewStyle: .favorites)
+        }.tabItem { Label("Favorites", systemImage: "heart.fill") }
+            SettingsView()
+                    .tabItem { Label("Settings", systemImage: "gear") }
+      }
+        .environmentObject(recipeData)
+        .onAppear {
+        recipeData.loadRecipes()
+     
+    }
+   
+    }
+}
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
